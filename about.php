17afffdf599d1de6ea5957c300f21843f96e6afe
TECHN0ID <nichham2@hotmail.com>
2020-10-12 23:24:46 -0500

    configured PHP pages footer and header added menue bar and index and about page. added bootstrap and javasript to the app

---
+++ about.php
@@ -0,0 +1,7 @@
+<?php 
+$title = 'About';
+require_once 'includes/header.php';
+?>
+    <h1>About</h1>
+    
+  <?php require_once 'includes/footer.php'; ?>
--- index.php
+++ index.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1> Hello World</h1>
-</body>
-</html>
+<?php 
+$title = 'Home';
+require_once 'includes/header.php';
+?>
+
+    <h1>Home</h1>
+    
+  <?php require_once 'includes/footer.php'; ?>
